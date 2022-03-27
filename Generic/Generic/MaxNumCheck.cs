d7e3e0246a797ae451c72ec752a7ec1691d3c3ef
kalpanabanjare <kalpanabanjare99@gmail.com>
2022-03-27 10:34:52 +0530

    Added: Max integer number

---
+++ Generic/Generic/MaxNumCheck.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generic
+{
+    public class MaxNumCheck
+    {
+        public static int MaximunIntegerNumber(int firstNumber, int secondNumber, int thirdNumber)
+        {
+            Console.WriteLine("Maximum Number for integer value");
+            if (firstNumber.CompareTo(secondNumber) > 0 && secondNumber.CompareTo(thirdNumber) > 0)
+                return firstNumber;
+
+            if (secondNumber.CompareTo(firstNumber) > 0 && secondNumber.CompareTo(thirdNumber) > 0)
+            {
+                return secondNumber;
+            }
+            if (secondNumber.CompareTo(firstNumber) > 0 && thirdNumber.CompareTo(secondNumber) > 0)
+            {
+                return thirdNumber;
+            }
+            throw new Exception("firstNumber,secondNumberand and secondNumber are equal.");
+        }
+    }
+}
---
+++ Generic/Generic/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace Generic
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Find maximum");
+            int output = MaxNumCheck.MaximunIntegerNumber(22, 66, 99);
+            Console.WriteLine(output);            
+        }
+    } 
+}    
